37f7fe8275a0270d8a1e9a4e1a3278c457de5ceb
Adrian Burlacu <adrian.burlacu@live.com>
2021-09-06 15:19:08 +0300

    Provide the server's config for the browser.

--- postinstall.js
+++ postinstall.js
@@ -21,6 +21,32 @@ async function Main() {
 	console.log("3. Copying public files.");
 	fs.copySync('public', './dist/public');
 	console.log("Copied public files.");
+  console.log("");
+  
+  console.log("4. Generating client config.");
+  const dotenv = require('dotenv');
+  dotenv.config();
+
+  const updateDotenv = require('@growflow/update-dotenv');
+  await updateDotenv.default({
+    'STARK_HOST': process.env['STARK_HOST'],
+    'STARK_PORT': process.env['STARK_PORT'],
+    'STARK_DB_HOST': process.env['STARK_DB_HOST'],
+    'STARK_USER_NAME': process.env['STARK_USER_NAME'],
+    'STARK_USER_PASSWORD': process.env['STARK_USER_PASSWORD'],
+    'STARK_USER_KEY': process.env['STARK_USER_KEY']
+  }, 'client');
+  
+  require('browser-env-vars').generate({
+    esm: true,
+    readFile: '.env.client'
+  });
+	console.log("Generated client config.");
+	console.log("");
+
+  console.log("5. Copying client config.");
+	fs.copySync('config.js', './dist/public/config.js');
+	console.log("Copied client config.");
 	console.log("");
 }
 Main();
