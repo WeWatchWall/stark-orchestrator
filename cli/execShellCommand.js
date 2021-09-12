43baf45ca4268ea5f099ced2ee9c9a270a834719
Adrian Burlacu <adrian.burlacu@live.com>
2021-09-12 12:15:56 +0300

    Spacing.

--- cli/execShellCommand.js
+++ cli/execShellCommand.js
@@ -5,15 +5,15 @@
  */
 function execShellCommand(cmd) {
  const exec = require('child_process').exec;
- return new Promise((resolve, reject) => {
-  exec(cmd, (error, stdout, stderr) => {
-   if (error) {
-    console.warn(error);
-    reject(error);
-   }
-   resolve(stdout? stdout : stderr);
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.warn(error);
+        reject(error);
+      }
+      resolve(stdout? stdout : stderr);
+    });
   });
- });
 }
 
 module.exports = execShellCommand;
--- cli/execShellCommand.ts
+++ cli/execShellCommand.ts
@@ -4,16 +4,16 @@
  * @return {Promise<string>}
  */
 function execShellCommand(cmd) {
- const exec = require('child_process').exec;
- return new Promise((resolve, reject) => {
+  const exec = require('child_process').exec;
+  return new Promise((resolve, reject) => {
   exec(cmd, (error, stdout, stderr) => {
-   if (error) {
-     console.warn(error);
-     reject(error);
-   }
-   resolve(stdout? stdout : stderr);
+    if (error) {
+      console.warn(error);
+      reject(error);
+    }
+    resolve(stdout? stdout : stderr);
   });
- });
+  });
 }
 
 module.exports = execShellCommand;
