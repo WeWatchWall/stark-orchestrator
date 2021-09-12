afc5cffe47c4946a3e268338828a7e59b9d9bbb0
Adrian Burlacu <adrian.burlacu@live.com>
2021-09-12 12:11:04 +0300

    Throw on shell error D:

--- cli/execShellCommand.ts
+++ cli/execShellCommand.ts
@@ -8,7 +8,8 @@ function execShellCommand(cmd) {
  return new Promise((resolve, reject) => {
   exec(cmd, (error, stdout, stderr) => {
    if (error) {
-    console.warn(error);
+     console.warn(error);
+     reject(error);
    }
    resolve(stdout? stdout : stderr);
   });
