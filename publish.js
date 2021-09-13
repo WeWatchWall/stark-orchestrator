4278a2a5435faad044de247a2efe72ca81deef83
Adrian Burlacu <adrian.burlacu@live.com>
2021-09-13 12:54:08 +0300

    Derp publish fix.

--- publish.js
+++ publish.js
@@ -5,6 +5,7 @@ async function publish() {
   const latestVersion = await import('latest-version');
   let version = await latestVersion.default(packName);
   
+  console.log(await execShellCommand('npm publish'));
   console.log(await execShellCommand(`npm unpublish ${packName}@${version}`));
 }
 publish();
