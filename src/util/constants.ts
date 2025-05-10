0239b5082ba6bc2a534337f1a875477ae88400e5
Adrian Burlacu <adrian.burlacu@live.com>
2025-05-10 12:01:04 +0300

    chore: cleanup constants.

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,3 +1,1 @@
-export const DEFAULT_ADMIN_USER_NAME = "admin@localhost.com";
-export const DEFAULT_ADMIN_USER_PASSWORD = "adminpassword";
 export const SESSION_EXPIRY = 1000 * 60 * 60 * 24; // 1 day in milliseconds
